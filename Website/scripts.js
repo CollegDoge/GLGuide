ed5c833c5366c3aab718690158859f21033303a9
CollegDoge <22156@northcote.school.nz>
2025-03-06 17:36:49 +1300

    More work, really just learning again. Using flex instead of grid containers.

--- Website/scripts.js
+++ Website/scripts.js
@@ -18,3 +18,14 @@ document.addEventListener("DOMContentLoaded", () => { // lets the page load befo
         }
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () { // Adds transparency to header on scroll
+    window.addEventListener("scroll", function () {
+        const header = document.querySelector("header");
+        if (window.scrollY > 50) {
+            header.classList.add("scrolled");
+        } else {
+            header.classList.remove("scrolled");
+        }
+    });
+});
